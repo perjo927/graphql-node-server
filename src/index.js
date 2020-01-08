6977b92406e04a0e902a7dd6794ce57832a58b60
perjo927 <djpjgj@gmail.com>
2020-01-08 16:08:45 +0100

    init: authentication

--- src/index.js
+++ src/index.js
@@ -1,49 +1,26 @@
+const Query = require("./resolvers/Query")
+const Mutation = require("./resolvers/Mutation")
+const User = require("./resolvers/User")
+const Link = require("./resolvers/Link")
+const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [{
-    id: "link-0",
-    url: "http://www.test.com",
-    description: "Foobar"
-}]
-
-let idCount = links.length
-
 const resolvers = {
-    Query: {
-        info: () => "This is some info",
-        feed: () => links,
-        link: (parent, { id }) => links.find(link => link.id === id)
-    },
-
-    Mutation: {
-        post: (parent, { url, description }) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description,
-                url
-            }
-            links.push(link)
-
-            return link
-        },
-        updateLink: (parent, { id, url, description }) => {
-            const item = { id, url, description }
-            const index = links.findIndex(i => i.id === item.id);
-            if (index === -1) return
-            links[index] = item
-            return links[index]
-        },
-        deleteLink: (parent, { id }) => {
-            const index = links.findIndex(i => i.id === id)
-            const [removedLink] = links.splice(index, 1)
-            return removedLink;
-        }
-    },
+    Query,
+    Mutation,
+    User,
+    Link
 }
 
 const server = new GraphQLServer({
     typeDefs: "./src/schema.graphql",
     resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma
+        }
+    }
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
